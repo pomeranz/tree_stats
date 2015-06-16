f7223120c1b8cb4c68c7251b7b3df41f40cb2e3f
pomeranz <pomeranz@ebi.ac.uk>
2015-06-16 13:39:50 +0100

    Made it a function

--- Scripts/tree_stats.py
+++ Scripts/tree_stats.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jun 12 16:49:53 2015
+
+@author: gideon
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Fri Jun 12 16:28:29 2015
 
 @author: gideon
 
-###                         TREE_STATS_V2                                   ###
+###                         TREE_STATS_V3                                   ###
+
+                        NOT WORKING YET
 
 Description:
 
-This script takes all the trees from the directory on 
-ebi-004 /nfs/research2/goldman/gregs/slr_pipeline/data/ens/78/trees
-It applies a few statistics onto it including:
+This script defines a function that collects some statistics from a tree 
+directory. The directory needs to be passed to the function. 
 
 -total tree length
 -minimum/maximum branch length
@@ -32,13 +40,14 @@ Change FileName, to name the output file.
 
 ## INPUT 
 
-Directory = "/home/gideon/Documents/mphil_internship/Trees/*/*" 
+#Directory = "/home/gideon/Documents/mphil_internship/Trees/*/*" 
 
 # It will save the file in the current directory
-FileName = "tree_stats_output.csv"
+#FileName = "tree_stats_output.csv"
+    
 
-#-----------------------------------------------------------------------------#
-
+    
+    
 ## PACKAGES
 
 # to change directories etc..
@@ -54,133 +63,144 @@ import csv
 
 #-----------------------------------------------------------------------------#
 
-## LOOP PREPARATION
+### THE FUNCTION ###
 
-# create a regexp to match for later
-ens_RE = re.compile("[A-Z]*")
+def tree_stats(directory, filename="tree_stats_output.csv"):
+    
+    ## LOOP PREPARATION
 
-# tree list to hold the final output. 
-tree_list = list() 
+    # create a regexp to match for later
+    ens_RE = re.compile("[A-Z]*")
 
-#-----------------------------------------------------------------------------#
-
-## LOOP
-
-for p in glob.glob(Directory):
-    
-    # list for that particular tree
-    tree = list()
-    
-    # acces the directory of the tree file
-    current_tree_directory = p
-    
-    # create ete tree object
-    current_tree = Tree(newick = current_tree_directory)
-    
-    # Add tree directory for identification
-    tree.append(current_tree_directory)
-    
-    
-    ## TREE LENGTH + MAX/MIN BRANCH LENGTH
-    
-    max_dist = 0.0
-    tree_length = 0.0
-    for n in current_tree.traverse():
-        tree_length += n.dist
-        if n.dist > max_dist:
-            max_dist = n.dist
-                        
-    # add tree length
-    tree.append(tree_length)
-    
-    # add max branch length
-    tree.append(max_dist)
-    
-    # calculate min dist
-    
-    min_dist = 10000.0
-    for n in current_tree.traverse():
-        if n.dist < min_dist:
-            min_dist = n.dist
-    
-    # add minimum branch length
-    tree.append(min_dist)
-    
-    
-    ## MAX/MIN BRANCH LENGTHS FROM ROOT
-    
-    # max length
-    max_leaf = current_tree.get_farthest_leaf()
-    #add to list
-    tree.append(max_leaf)
-    
-    # min length
-    min_leaf = current_tree.get_closest_leaf()
-    # add to list
-    tree.append(min_leaf)
-    
-    
-    # NUMBER OF LEAVES
-    
-    # calculate number of leaves
-    no_leaves = len(current_tree.get_leaves())
-    # add info to tree list
-    tree.append(no_leaves)
-    
-    
-    # NUMBER OF DIFFERENT SPECIES
-    
-    # save all the names in an object
-    leaf_names = current_tree.get_leaf_names()
-    
-    # use regexp to extract only species ids
-    species_ids = [ ens_RE.match(s).group(0) for s in leaf_names ]
-    
-    unique_species_ids = list(set(species_ids))
-    
-    no_species = len(unique_species_ids)
-    
-    # add to list
-    tree.append(no_species)
-    
-    
-    ## NUMBER OF PARALOGS
-    
-    # paralogs are number of leaves - number of sepcies
-    no_paralogs = no_leaves - no_species
-    
-    # add to list
-    tree.append(no_paralogs)
-    
-    ## NUMBER OF HUMAN SEQUENCES
-    
-    # count the number of only ENS for human seqs
-    human_seqs = species_ids.count("ENS")
-    
-    if human_seqs == 0:
-        no_human_seqs = 0
-        tree.append(no_human_seqs)
-    else:
-        no_human_seqs = len(human_seqs)
-        tree.append(no_human_seqs)
-    
-    
-    ## ADD TREE TO TREE LIST
-    tree_list.append(tree)
-    
-    ## END OF LOOP
+    # tree list to hold the final output. 
+    tree_list = list() 
     
 #-----------------------------------------------------------------------------#
-
-## WRITE TO FILE
-
-# create the column names
-colnames = ["Directory", "Length", "Biggest Branch", "Smallest Branch", 
-              "Farthest Leaf", "Closest Leaf", "# of Leaves", "# of Species", 
-              "# of Paralogs", "# of Human seqs"]
-
-# wrtite it to a file
-with open(FileName, 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",", dialect="excel")
-    writer.writerow(colnames)
-    writer.writerows(tree_list)
+    
+    ## LOOP
+    
+    for p in glob.glob(directory):
+        
+        # list for that particular tree
+        tree = list()
+        
+        # acces the directory of the tree file
+        current_tree_directory = p
+        
+        # create ete tree object
+        current_tree = Tree(newick = current_tree_directory)
+        
+        # Add tree directory for identification
+        tree.append(current_tree_directory)
+        
+        
+        ## TREE LENGTH + MAX/MIN BRANCH LENGTH
+        
+        max_dist = 0.0
+        tree_length = 0.0
+        for n in current_tree.traverse():
+            tree_length += n.dist
+            if n.dist > max_dist:
+                max_dist = n.dist
+                            
+        # add tree length
+        tree.append(tree_length)
+        
+        # add max branch length
+        tree.append(max_dist)
+        
+        # calculate min dist
+        
+        min_dist = 10000.0
+        for n in current_tree.traverse():
+            if n.dist < min_dist:
+                min_dist = n.dist
+        
+        # add minimum branch length
+        tree.append(min_dist)
+        
+        
+        ## MAX/MIN BRANCH LENGTHS FROM ROOT
+        
+        # max length
+        max_leaf = current_tree.get_farthest_leaf()
+        #add to list
+        tree.append(max_leaf)
+        
+        # min length
+        min_leaf = current_tree.get_closest_leaf()
+        # add to list
+        tree.append(min_leaf)
+        
+        
+        # NUMBER OF LEAVES
+        
+        # calculate number of leaves
+        no_leaves = len(current_tree.get_leaves())
+        # add info to tree list
+        tree.append(no_leaves)
+        
+        
+        # NUMBER OF DIFFERENT SPECIES
+        
+        # save all the names in an object
+        leaf_names = current_tree.get_leaf_names()
+        
+        # use regexp to extract only species ids
+        species_ids = [ ens_RE.match(s).group(0) for s in leaf_names ]
+        
+        unique_species_ids = list(set(species_ids))
+        
+        no_species = len(unique_species_ids)
+        
+        # add to list
+        tree.append(no_species)
+        
+        
+        ## NUMBER OF PARALOGS
+        
+        # paralogs are number of leaves - number of sepcies
+        no_paralogs = no_leaves - no_species
+        
+        # add to list
+        tree.append(no_paralogs)
+        
+        ## NUMBER OF HUMAN SEQUENCES
+        
+        # count the number of only ENS for human seqs
+        human_seqs = species_ids.count("ENS")
+        
+        if human_seqs == 0:
+            no_human_seqs = 0
+            tree.append(no_human_seqs)
+        else:
+            no_human_seqs = len(human_seqs)
+            tree.append(no_human_seqs)
+        
+        
+        ## ADD TREE TO TREE LIST
+        tree_list.append(tree)
+        
+        ## Show progress
+        print("Current file:" + current_tree_directory)        
+        
+        
+        ## END OF LOOP
+        
+#-----------------------------------------------------------------------------#
+    
+    ## WRITE TO FILE
+    
+    # create the column names
+    colnames = ["Directory", "Length", "Biggest Branch", "Smallest Branch", 
+                  "Farthest Leaf", "Closest Leaf", "# of Leaves", 
+                  "# of Species", "# of Paralogs", "# of Human seqs"]
+    
+    # wrtite it to a file
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", dialect="excel")
+        writer.writerow(colnames)
+        writer.writerows(tree_list)
+        
+    ## Return
+    return("Finished, the file output is in your current worknig directory")

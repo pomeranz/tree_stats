cb85e97d9effa80a7236a8111a1298531129d4b2
pomeranz <pomeranz@ebi.ac.uk>
2015-07-13 13:13:23 +0100

    Version 2, not working

---
+++ Scripts/fasta_prefix2.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 11:21:04 2015
+
+@author: gideon
+"""
+
+from Bio import SeqIO
+from argparse import ArgumentParser
+from ete2 import Tree
+# import re 
+from glob import glob
+
+
+argparser = ArgumentParser()
+
+# commands for function
+argparser.add_argument("--fasta", metavar="Fasta files directory", type=str, required=True)
+argparser.add_argument("--tree", metavar="Tree files directory", type=str, required=True)
+
+
+def fasta_prefix(fasta, tree):
+    
+    tree_dir = ''.join([tree + "*/*.nh"])
+    fasta_dir = ''.join([fasta + "*/*prank.best.fas"])
+    
+    tree_files = sorted(glob(tree_dir))
+    fasta_files = sorted(glob(fasta_dir))
+    
+    for file in range(len(fasta_files)-1):
+        
+        # print progress
+        print("".join("Finished " + str(file + 1) + " out of " + str(len(fasta_files))))        
+        
+        current_fasta = fasta_files[file]
+        current_tree = tree_files[file]
+        
+        with open (current_fasta, "r+") as fas:
+            for line in fas:
+                if line.startswith(">"):
+                    line = line[1:] # skip > character
+                    
+                    tree_ids = Tree(newick=current_tree)
+                    
+                    for tree_id in tree_ids.iter_leaf_names():
+                        if line in tree_id:
+                            print True
+        
+                            new_line = tree_id
+                            print line
+                            line.replace(line, new_line)
+                            break
+        fas.close()
+        
+args = argparser.parse_args()
+
+fasta = args.fasta
+tree = args.tree
+
+fasta_prefix(fasta,tree)
+
+print("All done, the fasta file names should match the tree IDs")
+
+
+
+
+"""
+for ID in SeqIO.parse(current_fasta,"fasta"):
+     tree_ids = Tree(newick=current_tree)
+     for tree_id in tree_ids.iter_leaf_names():
+         print ID.id
+         if ID.id in tree_id:
+             ID.id = tree_id
+             print(ID.id) 
+
+
+
+
+
+
+file = "/home/gideon/Documents/mphil_internship/prank_out/11_1_prank.best.fas"
+
+fast1 = SeqIO.parse(file, "fasta")
+
+for seq in SeqIO.parse(file, "fasta"):
+        
+        print seq.id
+        #SeqIO.write(seq_record, "/Users/bucephalus/Desktop/nc_pimps/raw/"+name[0]+".fas", "fasta")
+       
+
+Read in both directories
+Within directory read in correct files. 1_1.nh and 1_1.fas
+
+create a list with all the leaf names from the nh file.
+
+Loop trough fasta headers.
+
+for each header create a regexp and search for it within the list.
+The regexp greps out the 3 characters before the match = D1_ or D2_ 
+add this to the current header.
+Next header.
+
+Next fasta and nh file
+Next directory.
+
+Done!
+"""

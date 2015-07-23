e6cb99c69b4c446be1f351128eaab8e289c0dc68
pomeranz <pomeranz@ebi.ac.uk>
2015-07-23 13:09:05 +0100

    Finsihed

--- Scripts/tdg09_bsub.py
+++ Scripts/tdg09_bsub.py
@@ -17,7 +17,7 @@ from glob import glob
 import os
 from os import path
 import argparse
-import re
+#import re
 
 from utils import check_dir
 
@@ -27,7 +27,7 @@ argparser.add_argument("--alignment", metavar="Alignment file", type=str, requir
 argparser.add_argument("--groups", metavar="Prefixes", type=str, required=True)
 argparser.add_argument("--threads", metavar="No. of threads", type=str, required=True)
 argparser.add_argument("--tree", metavar="Tree file", type=str, required=True)
-#argparser.add_argument('--logdir', metavar='log_dir', type=str, required=True)
+argparser.add_argument('--logdir', metavar='log_dir', type=str, required=True)
 argparser.add_argument('--outroot', metavar='out_root', type=str, required=True)
 #argparser.add_argument('--rerun', action='store_true')
 
@@ -35,17 +35,14 @@ argparser.add_argument('--outroot', metavar='out_root', type=str, required=True)
 
 args = argparser.parse_args()
 
-tdg09_cmd = "java -cp dist/tdg09.jar tdg09.Analyse -alignment {0} -tree {1} -groups {2} -threads {3} > {4}"
+tdg09_cmd = "'java -cp dist/tdg09.jar tdg09.Analyse -alignment {0} -tree {1} -groups {2} -threads {3} > {4}'"
 
 alignment = args.alignment
 groups = args.groups
 threads = args.threads
 treeroot = args.tree
-#logroot = args.logdir
+logroot = args.logdir
 outroot = args.outroot
-
-#check_dir(logroot)
-check_dir(outroot)
 
 
 for infile in glob(path.join(alignment, "*", "*_prank.best.fas")):
@@ -58,12 +55,18 @@ for infile in glob(path.join(alignment, "*", "*_prank.best.fas")):
         check_dir(outdir)
         outfile = path.join(outdir, basename + ".txt")
         
+        logdir = path.join(logroot, args.clade, prefix)
+        check_dir(logroot)
+        
+        logfile = path.join(logdir, basename + '.log')
+        errfile = path.join(logdir, args.clade, basename + '.err')
+        
         treedir = path.join(treeroot, prefix)
         treefile = path.join(treedir, basename + '.nh')
         
         tdg09 = tdg09_cmd.format(infile, treefile, groups, threads, outroot)        
         
-        p = Popen(['bsub', '-R', 'rusage[tmp=512]',
+        p = Popen(['bsub', '-R', 'rusage[tmp=512]', '-o'+logfile,
                '-cwd /tmp', tdg09])
         
         p.wait()

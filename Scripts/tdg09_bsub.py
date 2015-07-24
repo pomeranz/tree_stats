b38b1e5e0ef97155b0c8b384e5def54f2826de01
pomeranz <pomeranz@ebi.ac.uk>
2015-07-24 23:58:00 +0100

    Finsihed

--- Scripts/tdg09_bsub.py
+++ Scripts/tdg09_bsub.py
@@ -66,7 +66,7 @@ for infile in glob(path.join(alignment, "*", "*_prank.best.fas")):
         
         tdg09 = tdg09_cmd.format(infile, treefile, groups, threads, outfile)        
         
-        p = Popen(['bsub', '-R', 'rusage[tmp=512]', '-o'+logfile, 
+        p = Popen(['bsub', '-n 8','-R', 'rusage[tmp=512]', '-o'+logfile, 
                    tdg09])
         
         p.wait()

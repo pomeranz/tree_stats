1cfe8e77a39158feb56efebfd8ea1f4e4327b7da
pomeranz <pomeranz@ebi.ac.uk>
2015-09-18 15:27:13 +0100

    Doing clever way of turning numbered tree back to tree with ids

--- Scripts/prepare_fubar.py
+++ Scripts/prepare_fubar.py
@@ -13,7 +13,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from dendropy import Tree
-from ete2 import Tree
+
 
 # Generate FASTA files
 # Copy trees over?
@@ -64,20 +64,27 @@ for f in glob.glob(path.join(args.indir, args.clade,
     out_tree = path.abspath(path.join(args.outdir, args.clade, basename[:2], input_core+'.nwk'))
     SeqIO.write(seqs, out_fasta, 'fasta')
     
-    tree = Tree.get_from_path(path.join(dirname, input_name+'.nwk'), 'newick', preserve_underscores=True)
+    with open(path.join(dirname, input_name+'.nwk')) as fl:
+        _ = fl.readline()
+        tree_string = fl.readline().rstrip()
+          
+    tree = Tree.get_from_string(tree_string, 'newick')    
+    
+    #tree = Tree.get_from_path(path.join(dirname, input_name+'.nwk'), 'newick', preserve_underscores=True)
     # rewrite the numbered tree back to a tree with ids. 
     # easy
     fasta_file = open(f, 'r')
     fasta = fasta_file.readlines()
     aln_ids = {}
     for index,line in enumerate(fasta,start=1):
-        
         if index%2 == 0:
-            aln_ids[index] = line
+            aln_ids[(index)/2] = line.strip("\n")
+            print line
+            print (index)/2
         
     for node in tree:
         if node.is_leaf():
-            node.taxon.label = aln_ids[node.taxon.label]
+            node.taxon.label = aln_ids[int(node.taxon.label)]
             
     
     #shutil.copy(path.join(dirname, input_name+'.nwk'),

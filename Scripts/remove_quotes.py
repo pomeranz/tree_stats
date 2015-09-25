fa16f1f219068a84c8dfbb76aa4bf37ffa751512
pomeranz <pomeranz@ebi.ac.uk>
2015-09-25 14:28:16 +0100

    added fixes to file so that fubar can run

--- Scripts/remove_quotes.py
+++ Scripts/remove_quotes.py
@@ -11,11 +11,13 @@ from glob import glob
 import os
 from os import path
 import argparse
+from dendropy import Tree
 
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('--treeroot', metavar='tree_root', type=str, required=True)
+argparser.add_argument('--fastaroot', metavar='fasta_root', type=str, required=True)
 
 args = argparser.parse_args()
 
@@ -23,9 +25,33 @@ for infile in glob(path.join(args.treeroot, "*", "*.nwk")):
     
     tree_file = open(infile, "r+")
     
+    basename = path.basename(infile).partition('.')[2]
+    prefix = basename.partition('_')[0][:2]
+    
     text = tree_file.read()
     text = text.replace("'", "")
     tree_file.seek(0)
     tree_file.write(text)
     tree_file.truncate()
     tree_file.close()
+    
+    tree = Tree.get_from_path(tree_file, 'newick', preserve_underscores=True)
+    
+    for node in tree:
+        if "." in node.taxon.label:
+            node.taxon.label = node.taxon.label.replace(".", "_")
+    
+     tree_file.seek(0)
+     tree_file.write(tree)
+     tree_file.truncate()
+     tree_file.close
+    
+    
+    fasta_file = path.join(args.fastaroot, basename + "_prank.best.fas")
+    
+    text = fasta_file.read()
+    text = text.replace(".", "")
+    fasta_file.seek(0)
+    fasta_file.write(text)
+    fasta_file.trunctuate()
+    fasta_file.close

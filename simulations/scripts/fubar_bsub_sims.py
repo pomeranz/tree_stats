e5036e5b584428f6608348e66b9cad12c4be3095
pomeranz <pomeranz@ebi.ac.uk>
2015-11-25 13:40:12 +0000

    Be able to run simulation data

--- simulations/scripts/fubar_bsub_sims.py
+++ simulations/scripts/fubar_bsub_sims.py
@@ -6,11 +6,11 @@ import argparse
 import re
 
 import utils
+from utils import check_dir
 
 fubar_cmd = "~sparks/hyphy-hyphyqt/HYPHYMP {0}"
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--clade', metavar='clade_name', type=str, required=True)
 argparser.add_argument('--inroot', metavar='input_root', type=str, required=True)
 argparser.add_argument('--logdir', metavar='log_dir', type=str, required=True)
 
@@ -20,21 +20,32 @@ inroot = args.inroot
 logroot = args.logdir
 
 utils.check_dir(logroot)
-utils.check_dir(path.join(logroot, args.clade))
 
-for infile in glob(path.join(inroot, args.clade, "*", "*_FUBAR.ctl")):
-    print infile
+sizes = "Small", "Medium", "Big"
+species_numbers = "6species", "12species", "17species", "44species"
+
+# prepare each of the 12 directories with sequences for slr
+for species in species_numbers:
+    print species
+    check_dir(path.join(inroot,species))    
     
-    basename = path.basename(infile)
-    logdir = path.join(logroot, args.clade, basename[:2])
+    for size in sizes:
+        print size
+        check_dir(path.join(inroot, species, size))
 
-    utils.check_dir(logdir)
-
-    logfile = path.join(logdir, basename + '.log')
-
-    fubar = fubar_cmd.format(infile)
-
-    p = Popen(['bsub', '-R', 'rusage[tmp=512]', '-o'+logfile, '-g', '/fubar', fubar])
+        for infile in glob(path.join(inroot, species, size,  "*", "*_FUBAR.ctl")):
+            print infile
+            
+            basename = path.basename(infile)
+            logdir = path.join(logroot, basename[:2])
         
-    p.wait()
+            utils.check_dir(logdir)
+        
+            logfile = path.join(logdir, basename + '.log')
+        
+            fubar = fubar_cmd.format(infile)
+        
+            p = Popen(['bsub', '-R', 'rusage[tmp=512]', '-o'+logfile, '-g', '/fubar', fubar])
+                
+            p.wait()
     
